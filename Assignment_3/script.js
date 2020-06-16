502472cb505952053319aec0e569cdbdaa610d49
steeti <sht318@nyu.edu>
2020-06-16 15:37:44 +0400

    Header and Option

--- Assignment_3/script.js
+++ Assignment_3/script.js
@@ -5,3 +5,19 @@
 
 let a = document.getElementsByTagName("audio")[0];
 a.addEventListener("timeupdate", function() { console.log(a.currentTime); }, true);
+
+
+
+function openoption(evt, optiontype) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("opt");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+  tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(optiontype).style.display = "block";
+  evt.currentTarget.className += " active";
+}
