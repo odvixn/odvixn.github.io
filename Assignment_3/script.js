fa3f84cea43a106c2fab0abe41885af3dfdad6c0
odvixn <sample@example.com>
2020-06-16 19:39:18 +0900

    milestone

--- Assignment_3/script.js
+++ Assignment_3/script.js
@@ -1,2 +1,7 @@
 // window.onblur = function() { document.title = ":(" };
 // window.onfocus = function() { document.title = ":)" };
+
+
+
+let a = document.getElementsByTagName("audio")[0];
+a.addEventListener("timeupdate", function() { console.log(a.currentTime); }, true);
