5b51854bbbeae86acb1b2e8a34e7046485cdc335
odvixn <57341309+odvixn@users.noreply.github.com>
2020-06-21 17:40:55 +0900

    initial commit

---
+++ Assignment_4/script.js
@@ -0,0 +1,36 @@
+// Automatic Slideshow - change image every 4 seconds
+// Source: https://www.w3schools.com/Css/css_rwd_templates.asp
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "flex";  
+  setTimeout(carousel, 4000);    
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+// Source: https://www.w3schools.com/Css/css_rwd_templates.asp
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// Source: https://www.w3schools.com/Css/css_rwd_templates.asp
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
