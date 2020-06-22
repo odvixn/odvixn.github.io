848281c8ed0a4d23a74bf4eec6f60419e2db1bb9
yahiabeethoven <58369022+yahiabeethoven@users.noreply.github.com>
2020-06-22 14:32:36 +0200

    Create questions_answers.js

---
+++ Assignment_4/questions_answers.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        question : "What is consent?",
+        // image was taken from https://www.healthline.com/health/guide-to-consent
+        imgSrc : "img/ques1.jpg",
+        choiceA : "A conversation between partners about sexual preferences and desires.",
+        choiceB : "An agreement between people to engage in sexual activity.",
+        choiceC : "A verbal contract outlining exactly what sexual activities will take place.",
+        correct : "B"
+    },{
+        question : "Which of the following statements indicates that consent has been given?",
+        // image was taken from https://www.columbiaspectator.com/news/2020/01/27/here-are-seven-factors-that-influence-sexual-consent/
+        imgSrc : "img/ques2.png",
+        choiceA : "Yes, I want to",
+        choiceB : "Maybe, I'm not sure",
+        choiceC : "I guess if you really want to",
+        correct : "A"
+    },{
+        question : "When should you ask for consent?",
+        // image was taken from https://www.nytimes.com/2017/12/16/sunday-review/when-saying-yes-is-easier-than-saying-no.html
+        imgSrc : "img/ques3.jpg",
+        choiceA : "Every time you engage in intercouse.",
+        choiceB : "First time you engage with a new partner.",
+        choiceC : "If you are in a public place.",
+        correct : "A"
+    }
+];
