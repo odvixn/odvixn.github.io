7aa188e4e2da27659db6a6b638c35a39da252662
odvixn <57341309+odvixn@users.noreply.github.com>
2020-06-27 01:27:30 +0900

    portfolio initial commit

---
+++ resources/script/script.js
@@ -0,0 +1,39 @@
+let hash = location.hash.replace('#','');
+let options = [
+    // theme
+
+]
+
+// read options
+if (hash!="") {options=hash.split('+');};
+
+
+
+
+
+function preload() {
+    console.log('preload');
+}
+
+// Idea from https://taliacotton.com/
+// document.onmousemove = function(e){
+//     coords = document.getElementById("coords");
+//     elem = getInnermostHovered().outerHTML.toString()
+//     coords.innerText = elem.substr(0,elem.indexOf(">")+1);
+
+//     coords.style.display = "block";
+//     coords.style.top = e.clientY + 12 + "px";
+//     coords.style.left = e.clientX + 10 + "px";
+// }
+
+// Get element under mouse pointer
+// https://stackoverflow.com/questions/24538450
+function getInnermostHovered() {
+    var n = document.querySelector(":hover");
+    var nn;
+    while (n) {
+        nn = n;
+        n = nn.querySelector(":hover");
+    }
+    return nn;
+}
---
+++ resources/script/welcome.js
@@ -0,0 +1,34 @@
+// JavaScript Loaded, replace HTML content
+
+    let notice = document.getElementsByClassName('wc-notice')[0];
+    notice.innerText = "Do you want to customize your experience?";
+
+    let wrapper = document.getElementsByClassName('wc-btn-wrapper-nojs')[0];
+    wrapper.className = wrapper.className.replace("-nojs", "");
+    wrapper.innerHTML = `
+    <div class="wc-btn wc-yes">
+        Yes
+    </div>
+    <div class="wc-btn" onclick="window.location.href='main.html'">
+        <div class="wc-no">No</div>
+    </div>
+    `;
+
+
+// When the window looses focus, change tab title
+
+    window.onblur = function() { document.title = "(｡•́︿•̀｡) Come back!" };
+    window.onfocus = function() { document.title = "Welcome!" };
+
+
+// Display mouse coordination
+// Code from https://taliacotton.com/
+
+    document.onmousemove = function(e){
+        coords = document.getElementById("coords");
+        coords.style.display = "block";
+        coords.innerHTML = "(X:"+e.clientX+",Y:"+e.clientY+")";
+        coords.style.top = e.clientY + 10 + "px";
+        coords.style.left = e.clientX + 10 + "px";
+    }
+
