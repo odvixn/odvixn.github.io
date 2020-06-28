93393f8b86bd33436fbcc4f89fd38939fe7dc13d
odvixn <57341309+odvixn@users.noreply.github.com>
2020-06-29 01:04:44 +0900

    now functional without JavaScript

--- resources/script/script.js
+++ resources/script/script.js
@@ -8,37 +8,6 @@
 // // read options
 // if (hash!="") {options=hash.split('+');};
 
-// I did not want to use JavaScript for simple redirection but I don't think I can fix css in time.
-let gLink = [
-    'projects/project_1/index.html',
-    'projects/project_2/index.html',
-    'projects/project_3/index.html',
-    'projects/project_4/index.html'
-];
-
-let gDoc = [
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_1/readme.md',
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_2/README.md',
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_3/README.md',
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_4/README.md'
-]
-
-let gGit = [
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_1',
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_2',
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_3',
-    'https://github.com/odvixn/odvixn.github.io/blob/master/projects/project_4'
-]
-
-document.getElementsByClassName('slide_overlay')[0].style.visibility = "unset";
-let radios = document.getElementsByClassName('c__a');
-
-function getProj() {
-    for (i=0;i<radios.length;i++) {
-        if (radios[i].checked) {return radios.length-i-1}
-    }
-}
-
 // Idea from https://taliacotton.com/
 document.onmousemove = function(e){
     if (window.innerWidth < 650) {return;}
